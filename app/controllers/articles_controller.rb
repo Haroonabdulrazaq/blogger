694eca79b4cbf0d066b827aa7815743e07f20462
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-08-13 11:18:56 -0300

    Add article helper method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end  
@@ -12,10 +14,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
+    
     redirect_to article_path(@article)
   end
 end
