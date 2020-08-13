7bafd27fc35af1c04f84a77933d03ed1b1d923fa
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-08-13 11:37:33 -0300

    Add destroy method to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,13 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
     redirect_to article_path(@article)
   end
 end
