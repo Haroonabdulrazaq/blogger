fb9e9ea41e4f25f989636bdc9acbe56f1a029bb3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-14 09:36:15 +0100

    Add edit method toblogger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,12 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+
+    # redirect_to articles_path(@article)
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
