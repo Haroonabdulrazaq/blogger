639462d7e81919708e2352618ec69dd061b6c221
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-08-14 11:47:14 -0300

    Add tag display

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,10 @@ class Article < ApplicationRecord
       tag.name
     end.join(", ")
   end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
