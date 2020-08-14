65c8b1051a3adf3d920fc0a3c1518d0bd19cce9c
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-08-14 10:30:18 -0300

    Add tag model and view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
 end
