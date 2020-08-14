7b75e60aa16e957f8256ad0cc7cc13486eb6d78d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-14 11:41:04 +0100

    Add comments to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end  
 end
